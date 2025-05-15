8201947dd4bb80043ea7d53a5eab1e9ae632ebce
patn <71495731+patn3314@users.noreply.github.com>
2025-05-15 10:22:14 +0900

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// GitHub Pages で配信する場合はリポジトリ名を base に設定
-export default defineConfig({
-  base: '/presentation-timer/',  // ★リポジトリ名に合わせる
-  plugins: [react()],
-});
